5d71ac0d083850c89c84c5da5d6f831f83d81024
Atzin Escandia <escandiatech@gmail.com>
2023-08-16 16:48:43 +0200

    Readme repository

--- .history/src/app/component-example/component-example.component_20230816142825.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-@Component({
-  selector: 'app-dynamic-angular-grid-factory',
-  templateUrl: './component-example.component.html',
-})
-export class DynamicAngularGridFactory {
-  @Input() component: any;
-}
--- .history/src/app/component-example/component-example.component_20230816142920.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-@Component({
-  selector: 'app-component-example',
-  templateUrl: './component-example.component.html',
-})
-export class DynamicAngularGridFactory {
-  @Input() data: any;
-}
